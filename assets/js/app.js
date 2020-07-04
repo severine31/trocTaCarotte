69889ecd35732bc888db42d73b6fb62a40c8abd2
unknown <you@example.com>
2020-07-04 20:05:47 +0200

    Refactoring for css -> responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,6 @@ import '../css/app.css';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
+import 'bootstrap';
+
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
