9bd2a3c88a285bf127e975a417d7cb98932f95ce
unknown <you@example.com>
2020-07-20 16:36:16 +0200

    Add Profil Controller

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfilController extends AbstractController
 {
     /**
      * @Route("/profil/{id<[0-9]+>}", name="profil", methods={"GET"})
      */
-    public function index()
+    public function index(UserRepository $repo, $id) : Response
     {
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
