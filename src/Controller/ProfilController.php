f732087101be88e94613438f13dae21543a17e8e
unknown <you@example.com>
2020-07-20 16:35:08 +0200

    Add Profil Controller

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilController extends AbstractController
+{
+    /**
+     * @Route("/profil/{id<[0-9]+>}", name="profil", methods={"GET"})
+     */
+    public function index()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+}
